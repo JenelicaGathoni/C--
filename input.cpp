742521092d0654c5541559b08fa9002082bcee22
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2025-04-14 12:01:25 +0300

    Learned how to accept input from a user

---
+++ input.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using std::cout;
+using std::cin;
+
+int main(){
+    int age;
+    cout << "Yo Jenelle! How old are you? " ;
+    cin >> age;
+    cout << "Okay! So you are " << age << " years old!";
+
+
+}
